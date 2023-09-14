7a47a82cf0d7d75e30c3d03f21572371e7c1fe28
Eloi Charly <nandry556@gmail.com>
2023-09-14 17:06:13 +0200

    feat: create container, components for user

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { UserContainer } from "./containers";
+
 const App = () => {
   return (
     <div>
-      <h1>This is a template </h1>
-      <p> This il </p>
+      <UserContainer />
     </div>
   );
 };
